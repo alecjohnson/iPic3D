f2ef6917d17cfbeda7e39eca768cfc98039bc74c
eajohnson <e.alec.johnson@gmail.com>
2013-06-11 00:04:58 +0200

    added error.h for error diagnostics

--- utility/diagnostics.cpp
+++ utility/diagnostics.cpp
@@ -1,6 +1,7 @@
 
 #include <stdarg.h>
 #include "asserts.h"
+#include "errors.h"
 #include "debug.h"
 #include "TimeTasks.h"
 #include "../mpidata/MPIdata.h" // for rank
@@ -34,6 +35,46 @@ void assert_error(const char* file, int line, const char* func,
 implement_assert_errmsg(int,int);
 implement_assert_errmsg(const char*,const char*);
 implement_assert_errmsg(const string&,const string&);
+
+/** implementation of declarations in utility/errors.h **/
+
+/*
+ *  quit and optionally send email message
+ */
+void errmsg_printf_fileLine(const char *func, const char *file, int line_number,
+  const char *format, ...)
+{
+  FILE* fptr = stderr;
+  fflush(fptr);
+  va_list args;
+  va_start(args, format);
+  fprintf(fptr, "ERROR in function %s, file %s, line %d: \n\t",
+    func, file, line_number);
+  /* print out remainder of message */
+  vfprintf(fptr, format, args);
+  va_end(args);
+  // append terminating newline so user does not have to do it
+  fprintf(fptr, "\n");
+  fflush(fptr);
+
+  abort();
+}
+
+#include <iostream>
+using namespace std;
+#define implement_invalid_value_error(t1) \
+  void invalid_value_error_fileLine(const char* file, int line, const char* func, \
+    const char* type, const char* expr, t1 val) \
+  { \
+    std::cerr<< "ERROR in file " << file << ", line " << line  \
+      << ", function " << func  \
+      <<"\n\t" << type << " value: " << expr << " = " << val << endl; \
+      abort(); \
+  }
+
+implement_invalid_value_error(double);
+implement_invalid_value_error(int);
+implement_invalid_value_error(const char*);
 
 /** implementation of declarations in utility/debug.h **/
 
