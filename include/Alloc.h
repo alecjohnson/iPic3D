a6efa4f131b9ef6e870bf4dcad0d87fa8bbdd5a0
eajohnson <e.alec.johnson@gmail.com>
2013-07-25 19:27:18 +0200

    turned array classes into templates

--- include/Alloc.h
+++ include/Alloc.h
@@ -1,5 +1,14 @@
-/****** begin (i,j) arrays from Reger Ferrer and Vicenç Beltran ******/
+#ifndef IPIC_ALLOC_H
+#define IPIC_ALLOC_H
+#include <cstddef>
 
+/*
+    Array class devloped by Alec Johnson
+      consolidating arrays developed by 
+    Reger Ferrer, Vicenç Beltran, and Florentino Sainz
+      and earlier arrays written by
+    Jorge Amaya and Stefano Markidis.
+*/
 #define ALIGNMENT (64)
 #ifdef __INTEL_COMPILER
     #define ALIGNED(X) __assume_aligned(X, ALIGNMENT)
@@ -11,245 +20,20 @@
     #define AlignedAlloc(T, NUM) (new T[NUM]) 
 #endif
 
-template <class ElementType>
-class Rank1
-{
-    const size_t _N1;
-    ElementType  * __restrict__ const  _storage;
-
-public:
-
-    Rank1(size_t N1) : _N1(N1), _storage(AlignedAlloc(ElementType, N1)) {}
-
-    //Rank1( const Rank1& other ) : _N1( other._N1 ), _storage( other._storage ) {}
-
-    ElementType& operator()(size_t n1) const
-    {
-       ALIGNED(_storage);
-       return _storage[n1];
-    }
-
-    size_t dim1() const { return _N1; }
-
-    ~Rank1() { };
-};
-
-template <class ElementType>
-class Rank2
-{
-    const size_t  _N1, _N2;
-    ElementType * __restrict__ const _storage;
-
-
-public:
-    Rank2(size_t N1, size_t N2) : _N1(N1), _N2(N2), _storage(AlignedAlloc(ElementType, N1*N2)) {}
-
-    //Rank2( const Rank2& other ) : _N1( other._N1 ), _N2( other._N2 ), _storage( other._storage ) {}
-
-    ElementType& operator()(size_t n1, size_t n2) const
-    {
-       ALIGNED(_storage);
-       return _storage[n2+_N2*n1];
-    }
-
-    size_t dim1() const { return _N1; }
-    size_t dim2() const { return _N2; }
-    
-    void deleteArr() {
-       AlignedFree(_storage);
-    }
-
-    ~Rank2() { };
-};
-    
-template <class ElementType>
-class Rank3
-{
-    const size_t _N1, _N2, _N3;
-    ElementType *    const __restrict__ _storage;
-
-
-public:
-
-    Rank3(size_t N1, size_t N2, size_t N3) : _N1(N1), _N2(N2), _N3(N3),
-    _storage(AlignedAlloc(ElementType, N1*N2*N3)) {}
-
-    //Rank3( const Rank3& other ) : _N1( other._N1 ), _N2( other._N2 ), _N3( other._N3 ),
-    //_storage( other._storage ) {}
-
-    ElementType& operator()(size_t n1, size_t n2, size_t n3) const
-    {
-       ALIGNED(_storage);
-       return _storage[n3+_N3*(n2+_N2*n1)];
-    }
-
-    ~Rank3() { }
-
-    size_t dim1() const { return _N1; }
-    size_t dim2() const { return _N2; }
-    size_t dim3() const { return _N3; }
-
-    void deleteArr() {
-       AlignedFree(_storage);
-    }
-};
-
-template <class ElementType>
-class Rank4
-{
-    const size_t _N1, _N2, _N3, _N4;
-    ElementType* __restrict__ const _storage;
-
-public:
-
-    Rank4(size_t N1, size_t N2, size_t N3, size_t N4) : _N1(N1), _N2(N2), _N3(N3), _N4(N4),
-    _storage(AlignedAlloc(ElementType, N1*N2*N3*N4)) {}
-
-    //Rank4( const Rank4& other ) : _N1( other._N1 ), _N2( other._N2 ), _N3( other._N3 ), _N4( other._N4 ),
-    //_storage( other._storage ) {}
-
-    ElementType& operator()(size_t n1, size_t n2, size_t n3, size_t n4) const
-    {
-       ALIGNED(_storage);
-       return _storage[n4+_N4*(n3+_N3*(n2+_N2*n1))];
-    }
-
-    ~Rank4() { }
-
-    size_t dim1() const { return _N1; } 
-    size_t dim2() const { return _N2; }
-    size_t dim3() const { return _N3; }
-    size_t dim4() const { return _N4; }
-    
-    void deleteArr() { AlignedFree(_storage); }
-
-};
-
-/******** end (i,j) arrays from Reger Ferrer and Vicenç Beltran ******/
-
-/****** begin [i][j] arrays from Reger Ferrer and Vicenç Beltran ******/
-
-template <class ElementType>
-class Array1
-{
-    const size_t _N1;
-    ElementType  * __restrict__ const  _storage;
-
-public:
-    Array1(size_t N1, void * __restrict__ const storage) : _N1(N1),
-         _storage(reinterpret_cast<ElementType * __restrict__ const>(storage)){}
-
-    Array1(size_t N1) : _N1(N1), _storage(new ElementType[N1]){}
-
-    ElementType& operator[](size_t i) const
-    {
-        return _storage[i];
-    }
-
-};
-
-template <class ElementType>
-class Array2
-{
-    const size_t _N1, _N2;
-    ElementType * __restrict__ const _storage;
-
-public:
-    Array2(size_t N1, size_t N2, void *storage) : _N1(N1), _N2(N2),
-         _storage(reinterpret_cast<ElementType * __restrict__ const>(storage)){}
-
-    Array2(size_t N1, size_t N2) : _N1(N1), _N2(N2),
-         _storage(new ElementType[N1*N2]) {}
-
-    Array1<ElementType> operator[](size_t i) const
-    {
-        return Array1<ElementType>(_N2, _storage + i * _N2);
-    }
-    ElementType& operator()(size_t n1, size_t n2) const
-    {
-       ALIGNED(_storage);
-       return _storage[n2+_N2*n1];
-    }
-};
-
-template <class ElementType>
-class Array3
-{
-    const size_t _N1, _N2, _N3;
-    ElementType* __restrict__ const _storage;
-
-public:
-    Array3(size_t N1, size_t N2, size_t N3, void *storage) : _N1(N1), _N2(N2), _N3(N3),
-         _storage(reinterpret_cast<ElementType * __restrict__ const>(storage)) {}
-
-
-    Array3(size_t N1, size_t N2, size_t N3) : _N1(N1), _N2(N2), _N3(N3),
-         _storage(new ElementType(N1*N2*N3)) {}
-
-    Array2<ElementType> operator[](size_t i) const
-    {
-        return Array2<ElementType>(_N2, _N3, _storage + i * _N2 * _N3);
-    }
-    ElementType& operator()(size_t n1, size_t n2, size_t n3) const
-    {
-       ALIGNED(_storage);
-       return _storage[n3+_N3*(n2+_N2*n1)];
-    }
-};
-
-template <class ElementType>
-class Array4
-{
-    const size_t _N1, _N2, _N3, _N4;
-    ElementType* __restrict__ const _storage;
-
-public:
-    Array4(size_t N1, size_t N2, size_t N3, size_t N4, void *storage) : _N1(N1), _N2(N2), _N3(N3), _N4(N4),
-         _storage(reinterpret_cast<ElementType * __restrict__ const>(storage)) {}
-
-    Array4(size_t N1, size_t N2, size_t N3, size_t N4) : _N1(N1), _N2(N2), _N3(N3), _N4(N4),
-         _storage(new ElementType(N1*N2*N3*N4)) {}
-
-    Array3<ElementType> operator[](size_t i) const
-    {
-        return Array3<ElementType>(_N2, _N3, _N4, _storage + i * _N2 * _N3 * _N4);
-    }
-    ElementType& operator()(size_t n1, size_t n2, size_t n3, size_t n4) const
-    {
-       ALIGNED(_storage);
-       return _storage[n4+_N4*(n3+_N3*(n2+_N2*n1))];
-    }
-
-};
-
 /*** Array classes if dimensions are known at compile time. ***/
 
-template <size_t N, size_t M>
-class IntArray2D
+template <class type, size_t N, size_t M>
+class Array2D
 {
 public:
-   int data [N][M];
+   type data [N][M];
 };
 
-template <size_t N1, size_t N2, size_t N3>
-class IntArray3D
+template <class type, size_t N1, size_t N2, size_t N3>
+class Array3D
 {
 public:
-   int data [N1][N2][N3];
-};
-
-template <size_t N, size_t M>
-class DoubleArray2D
-{
-public:
-   double data [N][M];
-};
-
-template <size_t N1, size_t N2, size_t N3>
-class DoubleArray3D
-{
-public:
-   double data [N1][N2][N3];
+   type data [N1][N2][N3];
 };
 
 /******** end [i][j] arrays from Reger Ferrer and Vicenç Beltran ******/
@@ -294,18 +78,23 @@ template < class type > inline type **** newArray4(size_t sz1, size_t sz2, size_
 
 // methods to deallocate arrays
 //
-template < class type > inline void delArr1(type * arr) {
+template < class type > inline void delArr1(type * arr)
+{
   AlignedFree(arr);
 }
-template < class type > inline void delArr2(type ** arr, size_t sz1) {
+template < class type > inline void delArr2(type ** arr, size_t sz1)
+{
   delArr1(arr[0]);
   AlignedFree(arr);
 }
-template < class type > inline void delArr3(type *** arr, size_t sz1, size_t sz2) {
+template < class type > inline void delArr3(type *** arr, size_t sz1, size_t sz2)
+{
   delArr2(arr[0],sz2);
   AlignedFree(arr);
 }
-template < class type > inline void delArr4(type **** arr, size_t sz1, size_t sz2, size_t sz3) {
+template < class type > inline void delArr4(type **** arr,
+  size_t sz1, size_t sz2, size_t sz3)
+{
   delArr3(arr[0],sz2,sz3);
   AlignedFree(arr);
 }
@@ -322,53 +111,57 @@ template < class type > inline void delArr4(type **** arr, size_t sz1, size_t sz
 // reduces to computing a one-dimensional subscript used to access a
 // one-dimensional array.
 //
-struct Deref1
+template <class type>
+class Deref1
 {
-  double* __restrict__ const arr;
+  type* __restrict__ const arr;
   const size_t* const sizes;
   size_t k;
-  inline Deref1(double *const arr_in, size_t const* const sizes_in, size_t k_in) :
+ public:
+  inline Deref1(type *const arr_in, size_t const* const sizes_in, size_t k_in) :
     arr(arr_in),
     sizes(sizes_in),
     k(k_in) {}
-  inline double& operator[](size_t idx){
+  inline type& operator[](size_t idx){
     k *= sizes[0]; k += idx;
     ALIGNED(arr);
     return arr[k];
   }
 };
 
-struct Deref2
+template <class type>
+class Deref2
 {
-  double* __restrict__ const arr;
+  type* __restrict__ const arr;
   const size_t* const sizes;
   size_t k;
-  inline Deref2(double *const arr_in, size_t const*const sizes_in, size_t k_in) :
+  inline Deref2(type *const arr_in, size_t const*const sizes_in, size_t k_in) :
     arr(arr_in),
     sizes(sizes_in),
     k(k_in) {}
-  inline Deref1 operator[](size_t idx){
+  inline Deref1<type> operator[](size_t idx){
     k *= sizes[0]; k += idx;
-    return Deref1(arr, sizes+1, k);
+    return Deref1<type>(arr, sizes+1, k);
   }
 };
 
-struct Deref3
+template <class type>
+class Deref3
 {
-  double* __restrict__ const arr;
+  type* __restrict__ const arr;
   const size_t* const sizes;
   size_t k;
-  inline Deref3(double*const arr_in, size_t const*const sizes_in, size_t k_in) :
+  inline Deref3(type*const arr_in, size_t const*const sizes_in, size_t k_in) :
     arr(arr_in),
     sizes(sizes_in),
     k(k_in) {}
-  inline Deref2 operator[](size_t idx){
+  inline Deref2<type> operator[](size_t idx){
     k *= sizes[0]; k += idx;
-    return Deref2(arr, sizes+1, k);
+    return Deref2<type>(arr, sizes+1, k);
   }
 };
 
-// doubleArrN adopts an array allocated by newArrN
+// ArrN adopts an array allocated by newArrN
 //
 // The purpose of these classes is to provide more efficient
 // and more regulated access to array elements.  The idea is to
@@ -381,42 +174,78 @@ struct Deref3
 // - methods that use parallel arithmetic for omp and vectorized code
 
 // class that can adopt array allocated by newArr1
-class doubleArr1
+template <class type>
+class Arr1
 {
   private:
-    double* __restrict__ const arr;
+    type* __restrict__ arr;
     size_t sizes[3];
-  public:
-    inline doubleArr1(double* in, size_t s1) :
-      arr(in)
+    bool owner;
+  private:
+    void set_size(size_t s1)
     {
-      sizes[0] = 2; // not used
-      sizes[1] = s1; //get_size(in);
+      sizes[0] = 1;
+      sizes[1] = s1;
       sizes[2] = 0;
     }
-    inline double operator[](size_t idx){
+  public:
+    ~Arr1()
+    {
+      if(owner) AlignedFree(arr);
+    }
+    Arr1(size_t s1) :
+      owner(true)
+    {
+      set_size(s1);
+      arr = AlignedAlloc(type, s1);
+    }
+    inline Arr1(type* in, size_t s1) :
+      arr(in),
+      owner(false)
+    {
+      set_size(s1);
+    }
+    inline type operator[](size_t idx){
       ALIGNED(arr);
       return arr[idx];
     }
 };
 
 // class that can adopt array allocated by newArr2
-class doubleArr2
+template <class type>
+class Arr2
 {
   private:
-    double* __restrict__ const arr;
+    type* __restrict__ arr;
     size_t sizes[4];
-  public:
-    inline doubleArr2(double*const* in, size_t s1, size_t s2) :
-      arr(*in)
+    bool owner;
+  private:
+    void set_sizes(size_t s1, size_t s2)
     {
-      sizes[0] = 2; // not used
-      sizes[1] = s1; //get_size(in);
-      sizes[2] = s2; //get_size(*in);
+      sizes[0] = 2;
+      sizes[1] = s1;
+      sizes[2] = s2;
       sizes[3] = 0;
     }
-    inline Deref1 operator[](size_t idx){
-      return Deref1(arr, sizes+2, idx);
+  public:
+    ~Arr2()
+    {
+      if(owner) AlignedFree(arr);
+    }
+    Arr2(size_t s1, size_t s2) :
+      owner(true)
+    {
+      set_sizes(s1,s2);
+      arr = AlignedAlloc(type, s1*s2);
+    }
+    Arr2(type*const* in, size_t s1, size_t s2) :
+      arr(*in),
+      owner(false)
+    {
+      set_sizes(s1,s2);
+    }
+    inline Deref1<type> operator[](size_t idx){
+      return Deref1<type>(arr, sizes+2, idx);
     }
     size_t getidx(size_t n1, size_t n2) const
     {
@@ -424,32 +253,50 @@ class doubleArr2
         k *= sizes[2]; k += n2;
         return k;
     }
-    const double& get(size_t n1,size_t n2) const
+    const type& get(size_t n1,size_t n2) const
       { ALIGNED(arr); return arr[getidx(n1,n2)]; }
-    double& fetch(size_t n1,size_t n2)
+    type& fetch(size_t n1,size_t n2)
       { ALIGNED(arr); return arr[getidx(n1,n2)]; }
-    void set(size_t n1,size_t n2, double value)
+    void set(size_t n1,size_t n2, type value)
       { ALIGNED(arr); arr[getidx(n1,n2)] = value; }
 };
 
 // class that can adopt array allocated by newArr3
-class doubleArr3
+template <class type>
+class Arr3
 {
   private:
-    double* arr;
+    type* __restrict__ arr;
     size_t sizes[5];
-  public:
-    inline doubleArr3(double*const*const* in, size_t s1, size_t s2, size_t s3) :
-      arr(**in)
+    bool owner;
+  private:
+    void set_sizes(size_t s1, size_t s2, size_t s3)
     {
-      sizes[0] = 3; // not used
-      sizes[1] = s1; // get_size(in);
-      sizes[2] = s2; // get_size(*in);
-      sizes[3] = s3; // get_size(**in);
+      sizes[0] = 3;
+      sizes[1] = s1;
+      sizes[2] = s2;
+      sizes[3] = s3;
       sizes[4] = 0;
     }
-    inline Deref2 operator[](size_t idx){
-      return Deref2(arr, sizes+2, idx);
+  public:
+    ~Arr3()
+    {
+      if(owner) AlignedFree(arr);
+    }
+    Arr3(size_t s1, size_t s2, size_t s3) :
+      owner(true)
+    {
+      set_sizes(s1,s2,s3);
+      arr = AlignedAlloc(type, s1*s2*s3);
+    }
+    Arr3(type*const*const* in, size_t s1, size_t s2, size_t s3) :
+      arr(**in),
+      owner(false)
+    {
+      set_sizes(s1,s2,s3);
+    }
+    inline Deref2<type> operator[](size_t idx){
+      return Deref2<type>(arr, sizes+2, idx);
     }
     size_t getidx(size_t n1, size_t n2, size_t n3) const
     {
@@ -458,34 +305,52 @@ class doubleArr3
         k *= sizes[3]; k += n3;
         return k;
     }
-    const double& get(size_t n1,size_t n2,size_t n3) const
+    const type& get(size_t n1,size_t n2,size_t n3) const
       { ALIGNED(arr); return arr[getidx(n1,n2,n3)]; }
-    double& fetch(size_t n1,size_t n2,size_t n3)
+    type& fetch(size_t n1,size_t n2,size_t n3)
       { ALIGNED(arr); return arr[getidx(n1,n2,n3)]; }
-    void set(size_t n1,size_t n2,size_t n3, double value)
+    void set(size_t n1,size_t n2,size_t n3, type value)
       { ALIGNED(arr); arr[getidx(n1,n2,n3)] = value; }
 };
 
 // class that can adopt array allocated by newArr4
-class doubleArr4
+template <class type>
+class Arr4
 {
-  private:
-    double* arr;
+  private: // data
+    type* __restrict__ arr;
     size_t sizes[6];
-  public:
-    doubleArr4(double*const*const*const* in,
-      size_t s1, size_t s2, size_t s3, size_t s4) :
-      arr(***in)
+    bool owner;
+  private: // methods
+    void set_sizes(size_t s1, size_t s2, size_t s3, size_t s4)
     {
-      sizes[0] = 4; // not used
-      sizes[1] = s1; //get_size(in);
-      sizes[2] = s2; //get_size(*in);
-      sizes[3] = s3; //get_size(**in);
-      sizes[4] = s4; //get_size(***in);
+      sizes[0] = 4;
+      sizes[1] = s1;
+      sizes[2] = s2;
+      sizes[3] = s3;
+      sizes[4] = s4;
       sizes[5] = 0;
     }
-    inline Deref3 operator[](size_t idx){
-      return Deref3(arr, sizes+2, idx);
+  public:
+    ~Arr4()
+    {
+      if(owner) AlignedFree(arr);
+    }
+    Arr4(size_t s1, size_t s2, size_t s3, size_t s4) :
+      owner(true)
+    {
+      set_sizes(s1,s2,s3,s4);
+      arr = AlignedAlloc(type, s1*s2*s3*s4);
+    }
+    Arr4(type*const*const*const* in,
+      size_t s1, size_t s2, size_t s3, size_t s4) :
+      arr(***in),
+      owner(false)
+    {
+      set_sizes(s1,s2,s3,s4);
+    }
+    inline Deref3<type> operator[](size_t idx){
+      return Deref3<type>(arr, sizes+2, idx);
     }
     size_t getidx(size_t n1, size_t n2, size_t n3, size_t n4) const
     {
@@ -495,11 +360,11 @@ class doubleArr4
         k *= sizes[4]; k += n4;
         return k;
     }
-    const double& get(size_t n1,size_t n2,size_t n3,size_t n4) const
+    const type& get(size_t n1,size_t n2,size_t n3,size_t n4) const
       { return arr[getidx(n1,n2,n3,n4)]; }
-    double& fetch(size_t n1,size_t n2,size_t n3,size_t n4)
+    type& fetch(size_t n1,size_t n2,size_t n3,size_t n4)
       { return arr[getidx(n1,n2,n3,n4)]; }
-    void set(size_t n1,size_t n2,size_t n3,size_t n4, double value)
+    void set(size_t n1,size_t n2,size_t n3,size_t n4, type value)
       { arr[getidx(n1,n2,n3,n4)] = value; }
 };
 
@@ -507,3 +372,4 @@ class doubleArr4
 #define newArr3(type,sz1,sz2,sz3) newArray3<type>((sz1),(sz2),(sz3))
 #define newArr2(type,sz1,sz2) newArray2<type>((sz1),(sz2))
 /****** end Alec Johnson's code ******/
+#endif
--- tests/test_arrays.cpp
+++ tests/test_arrays.cpp
@@ -11,119 +11,254 @@
 #include "../include/Alloc.h"
 #include "assert.h"
 
+/****** begin (i,j) arrays from Reger Ferrer and Vicenç Beltran ******/
+
+template <class ElementType>
+class Rank1
+{
+    const size_t _N1;
+    ElementType  * __restrict__ const  _storage;
+
+public:
+
+    Rank1(size_t N1) : _N1(N1), _storage(AlignedAlloc(ElementType, N1)) {}
+
+    //Rank1( const Rank1& other ) : _N1( other._N1 ), _storage( other._storage ) {}
+
+    ElementType& operator()(size_t n1) const
+    {
+       ALIGNED(_storage);
+       return _storage[n1];
+    }
+
+    size_t dim1() const { return _N1; }
+
+    ~Rank1() { };
+};
+
+template <class ElementType>
+class Rank2
+{
+    const size_t  _N1, _N2;
+    ElementType * __restrict__ const _storage;
+
+
+public:
+    Rank2(size_t N1, size_t N2) : _N1(N1), _N2(N2), _storage(AlignedAlloc(ElementType, N1*N2)) {}
+
+    //Rank2( const Rank2& other ) : _N1( other._N1 ), _N2( other._N2 ), _storage( other._storage ) {}
+
+    ElementType& operator()(size_t n1, size_t n2) const
+    {
+       ALIGNED(_storage);
+       return _storage[n2+_N2*n1];
+    }
+
+    size_t dim1() const { return _N1; }
+    size_t dim2() const { return _N2; }
+    
+    void deleteArr() {
+       AlignedFree(_storage);
+    }
+
+    ~Rank2() { };
+};
+    
+template <class ElementType>
+class Rank3
+{
+    const size_t _N1, _N2, _N3;
+    ElementType *    const __restrict__ _storage;
+
+
+public:
+
+    Rank3(size_t N1, size_t N2, size_t N3) : _N1(N1), _N2(N2), _N3(N3),
+    _storage(AlignedAlloc(ElementType, N1*N2*N3)) {}
+
+    //Rank3( const Rank3& other ) : _N1( other._N1 ), _N2( other._N2 ), _N3( other._N3 ),
+    //_storage( other._storage ) {}
+
+    ElementType& operator()(size_t n1, size_t n2, size_t n3) const
+    {
+       ALIGNED(_storage);
+       return _storage[n3+_N3*(n2+_N2*n1)];
+    }
+
+    ~Rank3() { }
+
+    size_t dim1() const { return _N1; }
+    size_t dim2() const { return _N2; }
+    size_t dim3() const { return _N3; }
+
+    void deleteArr() {
+       AlignedFree(_storage);
+    }
+};
+
+template <class ElementType>
+class Rank4
+{
+    const size_t _N1, _N2, _N3, _N4;
+    ElementType* __restrict__ const _storage;
+
+public:
+
+    Rank4(size_t N1, size_t N2, size_t N3, size_t N4) : _N1(N1), _N2(N2), _N3(N3), _N4(N4),
+    _storage(AlignedAlloc(ElementType, N1*N2*N3*N4)) {}
+
+    //Rank4( const Rank4& other ) : _N1( other._N1 ), _N2( other._N2 ), _N3( other._N3 ), _N4( other._N4 ),
+    //_storage( other._storage ) {}
+
+    ElementType& operator()(size_t n1, size_t n2, size_t n3, size_t n4) const
+    {
+       ALIGNED(_storage);
+       return _storage[n4+_N4*(n3+_N3*(n2+_N2*n1))];
+    }
+
+    ~Rank4() { }
+
+    size_t dim1() const { return _N1; } 
+    size_t dim2() const { return _N2; }
+    size_t dim3() const { return _N3; }
+    size_t dim4() const { return _N4; }
+    
+    void deleteArr() { AlignedFree(_storage); }
+
+};
+
+/******** end (i,j) arrays from Reger Ferrer and Vicenç Beltran ******/
+
+/****** begin [i][j] arrays from Reger Ferrer and Vicenç Beltran ******/
+
+template <class ElementType>
+class Array1
+{
+    const size_t _N1;
+    ElementType  * __restrict__ const  _storage;
+
+public:
+    Array1(size_t N1, void * __restrict__ const storage) : _N1(N1),
+         _storage(reinterpret_cast<ElementType * __restrict__ const>(storage)){}
+
+    Array1(size_t N1) : _N1(N1), _storage(new ElementType[N1]){}
+
+    ElementType& operator[](size_t i) const
+    {
+        return _storage[i];
+    }
+
+};
+
+template <class ElementType>
+class Array2
+{
+    const size_t _N1, _N2;
+    ElementType * __restrict__ const _storage;
+
+public:
+    Array2(size_t N1, size_t N2, void *storage) : _N1(N1), _N2(N2),
+         _storage(reinterpret_cast<ElementType * __restrict__ const>(storage)){}
+
+    Array2(size_t N1, size_t N2) : _N1(N1), _N2(N2),
+         _storage(new ElementType[N1*N2]) {}
+
+    Array1<ElementType> operator[](size_t i) const
+    {
+        return Array1<ElementType>(_N2, _storage + i * _N2);
+    }
+    ElementType& operator()(size_t n1, size_t n2) const
+    {
+       ALIGNED(_storage);
+       return _storage[n2+_N2*n1];
+    }
+};
+
+template <class ElementType>
+class Array3
+{
+    const size_t _N1, _N2, _N3;
+    ElementType* __restrict__ const _storage;
+
+public:
+    Array3(size_t N1, size_t N2, size_t N3, void *storage) : _N1(N1), _N2(N2), _N3(N3),
+         _storage(reinterpret_cast<ElementType * __restrict__ const>(storage)) {}
+
+
+    Array3(size_t N1, size_t N2, size_t N3) : _N1(N1), _N2(N2), _N3(N3),
+         _storage(new ElementType(N1*N2*N3)) {}
+
+    Array2<ElementType> operator[](size_t i) const
+    {
+        return Array2<ElementType>(_N2, _N3, _storage + i * _N2 * _N3);
+    }
+    ElementType& operator()(size_t n1, size_t n2, size_t n3) const
+    {
+       ALIGNED(_storage);
+       return _storage[n3+_N3*(n2+_N2*n1)];
+    }
+};
+
+template <class ElementType>
+class Array4
+{
+    const size_t _N1, _N2, _N3, _N4;
+    ElementType* __restrict__ const _storage;
+
+public:
+    Array4(size_t N1, size_t N2, size_t N3, size_t N4, void *storage) : _N1(N1), _N2(N2), _N3(N3), _N4(N4),
+         _storage(reinterpret_cast<ElementType * __restrict__ const>(storage)) {}
+
+    Array4(size_t N1, size_t N2, size_t N3, size_t N4) : _N1(N1), _N2(N2), _N3(N3), _N4(N4),
+         _storage(new ElementType(N1*N2*N3*N4)) {}
+
+    Array3<ElementType> operator[](size_t i) const
+    {
+        return Array3<ElementType>(_N2, _N3, _N4, _storage + i * _N2 * _N3 * _N4);
+    }
+    ElementType& operator()(size_t n1, size_t n2, size_t n3, size_t n4) const
+    {
+       ALIGNED(_storage);
+       return _storage[n4+_N4*(n3+_N3*(n2+_N2*n1))];
+    }
+
+};
+
+/******** end [i][j] arrays from Reger Ferrer and Vicenç Beltran ******/
+
 using namespace std;
 
 const int ITERS = 10000;
 const size_t DIM_X = 64;
 const size_t DIM_Y = 64;
 
-void test_int_arrays()
+template <class type>
+void test_arrays()
 {
    const size_t n = DIM_X;
    const size_t m = DIM_Y;
 
-   Array2<int> Abra(n, m);
-   Array2<int> Bbra(n, m);
-   Array2<int> Cbra(n, m);
+   Array2<type> Abra(n, m);
+   Array2<type> Bbra(n, m);
+   Array2<type> Cbra(n, m);
 
-   Rank2<int> Apar(n, m);
-   Rank2<int> Bpar(n, m);
-   Rank2<int> Cpar(n, m);
+   Rank2<type> Apar(n, m);
+   Rank2<type> Bpar(n, m);
+   Rank2<type> Cpar(n, m);
 
-   IntArray2D<DIM_X, DIM_Y> Afix ;
-   IntArray2D<DIM_X, DIM_Y> Bfix ;
-   IntArray2D<DIM_X, DIM_Y> Cfix ;
+   Array2D<type, DIM_X, DIM_Y> Afix ;
+   Array2D<type, DIM_X, DIM_Y> Bfix ;
+   Array2D<type, DIM_X, DIM_Y> Cfix ;
 
-   int** Aold = newArr2(int, DIM_X, DIM_Y);
-   int** Bold = newArr2(int, DIM_X, DIM_Y);
-   int** Cold = newArr2(int, DIM_X, DIM_Y);
+   type** Aold = newArr2(type, DIM_X, DIM_Y);
+   type** Bold = newArr2(type, DIM_X, DIM_Y);
+   type** Cold = newArr2(type, DIM_X, DIM_Y);
+
+   Arr2<type> Aeaj(Aold, DIM_X, DIM_Y);
+   Arr2<type> Beaj(Bold, DIM_X, DIM_Y);
+   Arr2<type> Ceaj(Cold, DIM_X, DIM_Y);
 
    printf("Initializing data ...\n");
-   for(size_t i=0; i<DIM_X; i++){
-      for(size_t j=i; j<DIM_Y; j++){
-         Bbra[i][j] = rand();
-         Cbra[i][j] = rand();
-         Bpar(i,j) = Bbra[i][j];
-         Cpar(i,j) = Cbra[i][j];
-         Bfix.data[i][j] = Bbra[i][j];
-         Cfix.data[i][j] = Cbra[i][j];
-         Bold[i][j] = Bbra[i][j];
-         Cold[i][j] = Cbra[i][j];
-      }
-   }
-
-   stopwatch(START);
-   for(int t=0; t<ITERS; t++)
-   for(size_t i=0; i<DIM_X; i++){
-      for(size_t j=i; j<DIM_Y; j++){
-         Abra[i][j] = Bbra[i][j] * Cbra[i][j];
-      }
-   }
-   printf("Total time bracket array: %d ms\n", tv_to_ms(stopwatch(LAP)));
-
-   for(int t=0; t<ITERS; t++)
-   for(size_t i=0; i<DIM_X; i++){
-      for(size_t j=i; j<DIM_Y; j++){
-         Apar(i,j) = Bpar(i,j) * Cpar(i,j);
-      }
-   }
-   printf("Total time parentheses array: %d ms\n", tv_to_ms(stopwatch(LAP)));
-
-   for(int t=0; t<ITERS; t++)
-   for(size_t i=0; i<DIM_X; i++){
-      for(size_t j=i; j<DIM_Y; j++){
-         Afix.data[i][j] = Bfix.data[i][j] * Cfix.data[i][j];
-      }
-   }
-   printf("Total time fixed array: %d ms\n", tv_to_ms(stopwatch(LAP)));
-
-   for(int t=0; t<ITERS; t++)
-   for(size_t i=0; i<DIM_X; i++){
-      for(size_t j=i; j<DIM_Y; j++){
-         Aold[i][j] = Bold[i][j] * Cold[i][j];
-      }
-   }
-   printf("Total time indirect array: %d ms\n", tv_to_ms(stopwatch(LAP)));
-
-   for(size_t i=0; i<DIM_X; i++){
-      for(size_t j=i; j<DIM_Y; j++){
-         assert(Aold[i][j] == Abra[i][j]);
-         assert(Afix.data[i][j] == Abra[i][j]);
-      }
-   }
-
-   printf("Verification done!\n");
-   stopwatch(STOP);
-
-}
-
-void test_double_arrays()
-{
-   printf("=============================\n");
-   const size_t n = DIM_X;
-   const size_t m = DIM_Y;
-
-   Array2<double> Abra(n, m);
-   Array2<double> Bbra(n, m);
-   Array2<double> Cbra(n, m);
-
-   Rank2<double> Apar(n, m);
-   Rank2<double> Bpar(n, m);
-   Rank2<double> Cpar(n, m);
-
-   DoubleArray2D<DIM_X, DIM_Y> Afix ;
-   DoubleArray2D<DIM_X, DIM_Y> Bfix ;
-   DoubleArray2D<DIM_X, DIM_Y> Cfix ;
-
-   double** Aold = newArr2(double, DIM_X, DIM_Y);
-   double** Bold = newArr2(double, DIM_X, DIM_Y);
-   double** Cold = newArr2(double, DIM_X, DIM_Y);
-
-   doubleArr2 Aeaj(Aold, DIM_X, DIM_Y);
-   doubleArr2 Beaj(Bold, DIM_X, DIM_Y);
-   doubleArr2 Ceaj(Cold, DIM_X, DIM_Y);
-
-   printf("Initializing double data ...\n");
    for(size_t i=0; i<DIM_X; i++){
       for(size_t j=i; j<DIM_Y; j++){
          Bbra[i][j] = rand();
@@ -194,11 +329,13 @@ void test_double_arrays()
    }
 
    printf("Verification done!\n");
-
+   stopwatch(STOP);
 }
 
 int main()
 {
-  test_int_arrays();
-  test_double_arrays();
+  printf("=== testing integer arrays ===\n");
+  test_arrays<int>();
+  printf("=== testing double arrays ===\n");
+  test_arrays<double>();
 }
