e6c32f7efe0a7137da1db2fcc924f4d51ee4a87d
default_git_name <default_git_email>
2014-02-10 21:52:25 +0000

    PclGrid test program now working

--- tests/test_class_structure.cpp
+++ tests/test_class_structure.cpp
@@ -1,11 +1,14 @@
 #include "../include/debug.h"
+#include "../include/assert.h"
+#include "../include/ipicdefs.h" // for pfloat
+#include "../include/arraysfwd.h"
+#define NO_NEW
 #include "../include/Alloc.h"
 
-#if 0 // pseudo-code for particle processing
+using namespace iPic3D;
 
- Instantiate the following:
+#if 0
 
-   T   threads
    C+C communication buffers (6 <= C <= 26)
        designated as recv_buffer[direction] and send_buffer[direction].
        C=6 if communicating to face-neighbor processes and C=26
@@ -139,12 +142,13 @@ struct ImFieldSolver: public ImFieldSolver_interface
 class ImKineticSolver_interface
 {
  public:
-  void CalculateMoments() = 0;
-  void MovePcls(double dt) = 0;
+  virtual void CalculateMoments() = 0;
+  virtual void MovePcls(double dt) = 0;
 };
 
 class Grid
 {
+ protected:
   int nxc;
   int nyc;
   int nzc;
@@ -155,6 +159,13 @@ class Grid
   double yStart;
   double zStart;
  public:
+  Grid(int nxc_, int nyc_, int nzc_,
+    double dx_, double dy_, double dz_,
+    double xStart_, double yStart_, double zStart_)
+   :nxc(nxc_), nyc(nyc_), nzc(nzc_),
+    dx(dx_), dy(dy_), dz(dz_),
+    xStart(xStart_), yStart(yStart_), zStart(zStart_)
+  {}
   void get_cell_for_position(
     int& xc, int& yc, int& zc,
     double x, double y, double z);
@@ -173,7 +184,39 @@ class FieldGrid: public Grid
 template<class PclList>
 class PclGrid: public Grid
 {
+ public: // will be private
   array3<PclList> pclGrid;
+ public:
+  PclList& fetch(int i, int j, int k)
+  {
+    return pclGrid[i][j][k];
+  }
+  PclGrid(int nxc_, int nyc_, int nzc_,
+    double dx_, double dy_, double dz_,
+    double xStart_, double yStart_, double zStart_)
+   :Grid(nxc_, nyc_, nzc_, dx_, dy_, dz_,
+      xStart_, yStart_, zStart_),
+    pclGrid(nxc,nyc,nzc)
+  {}
+  PclGrid(int nxc_, int nyc_, int nzc_)
+   :Grid(nxc_, nyc_, nzc_, 1, 1, 1, 0, 0, 0),
+    pclGrid(nxc,nyc,nzc)
+  {}
+  // resize all particle lists to hold as much as npc particles
+  void resize(int npc)
+  {
+    for(int i=0;i<nxc;i++)
+    for(int j=0;j<nyc;j++)
+    for(int k=0;k<nzc;k++)
+      pclGrid[i][j][k].resize(npc);
+  }
+  void alloc(int npc)
+  {
+    for(int i=0;i<nxc;i++)
+    for(int j=0;j<nyc;j++)
+    for(int k=0;k<nzc;k++)
+      pclGrid[i][j][k].alloc(npc);
+  }
 };
 
 template<class PclList>
@@ -189,14 +232,87 @@ class PclSolver:
   }
 };
 
+// should support access of the form pclList[i].x,
+// which could in fact also be supported
+// with an underlying SoA representation.
+//
 template<class Pcl>
-class PclList_AoS<Pcl>
+class PclList_AoS
 {
-  array1<Pcl> pclList;
-}
+ private: // members
+  Pcl* pclList;
+  int last; // index of last particle in list
+  int maxsize; // maximum number of particles
+ public: // access
+  void pop()
+  {
+    assert(last>=0);
+    last--;
+    // return pclList[last--];
+  }
+  void push_back(const Pcl& pcl)
+  {
+    last++;
+    pclList[last] = pcl;
+    assert(last<maxsize);
+  }
+  inline Pcl& operator[](int i)
+  {
+    #ifdef CHECK_BOUNDS
+      assert_le(0, i);
+      assert_le(i, last);
+    #endif
+    ALIGNED(pclList);
+    return pclList[i];
+  }
+  void delete_pcl(int i)
+  {
+    // works even for last particle,
+    // though pop would be faster in that case.
+    // 
+    // why doesn't this compile?
+    //this->operator[i] = pclList[last--];
+    #ifdef CHECK_BOUNDS
+      assert_le(0, i);
+      assert_le(i, last);
+    #endif
+    ALIGNED(pclList);
+    pclList[i] = pclList[last--];
+  }
+ public: // memory
+  ~PclList_AoS()
+  {
+    AlignedFree(pclList);
+  }
+  PclList_AoS():
+    pclList(0),
+    last(-1),
+    maxsize(0)
+  {}
+  // truncate and reallocate list
+  void alloc(int in)
+  {
+    maxsize = in;
+    free(pclList);
+    pclList = AlignedAlloc(Pcl,maxsize);
+    last = -1;
+  }
+  // change maximum size without deleting pcls
+  void resize(int newmaxsize)
+  {
+    Pcl* oldPclList = pclList;
+    assert(last+1 < newmaxsize);
+    maxsize = newmaxsize;
+    for(int i=0;i<=last;i++)
+    {
+      pclList[i] = oldPclList[i];
+    }
+    AlignedFree(oldPclList);
+  }
+};
 
 // 512 bytes (cache-line-sized)
-class SpeciesPcl
+struct SpeciesPcl
 {
   double x;
   double y;
@@ -226,7 +342,7 @@ class ImPcl
   float m; // mass
   float qom; // charge-to-mass ratio
   long long ID;
-}
+};
 
 // 1024 bits, for 3rd-order mover
 //
@@ -248,3 +364,69 @@ class ImPcl3
   double qom; // charge-to-mass ratio
   long long ID;
 };
+
+//int test_arrays()
+//{
+//  array1_int arr(5);
+//  //int* arr = (int*)malloc(sizeof(int)*5);
+//  for(int i=0;i<5;i++)
+//    arr[i] = i;
+//  for(int i=0;i<5;i++)
+//    printf("arr[%d] = %d\n", i, arr[i]);
+//  array3<int> arr3(4,3,2);
+//  for(int i=0;i<4;i++)
+//  for(int j=0;j<3;j++)
+//  for(int k=0;k<2;k++)
+//    arr3[i][j][k] = i+j+k;
+//  for(int i=0;i<4;i++)
+//  for(int j=0;j<3;j++)
+//  for(int k=0;k<2;k++)
+//    printf("arr3[%d][%d][%d] = %d\n",i,j,k, arr3[i][j][k]);
+//}
+//
+//int test_PclList()
+//{
+//  // create grid of particles
+//  //PclGrid<PclList_AoS<SpeciesPcl>> pclGrid;
+//  PclList_AoS<SpeciesPcl> pclList;
+//  pclList.alloc(5);
+//  SpeciesPcl pcl0;
+//  SpeciesPcl pcl1;
+//  pclList.push_back(pcl0);
+//  pclList.push_back(pcl1);
+//  pclList[1].x = 1.234;
+//  printf("pclList[%d].x=%g\n", 1, pclList[1].x);
+//  pcl0.y = 2.345;
+//  pclList[0] = pcl0;
+//  printf("pclList[%d].y=%g\n", 0, pclList[0].y);
+//
+//  array1<SpeciesPcl> pcls(5);
+//  pcls[1].x = 5.342;
+//  printf("pcls[%d].x=%g\n", 1, pcls[1].x);
+//}
+
+int test_PclGrid()
+{
+  // create grid of particles
+  PclGrid<PclList_AoS<SpeciesPcl> > pclGrid(2,3,4);
+  pclGrid.alloc(2); // at most 2 particles per cell
+  SpeciesPcl pcl;
+  pcl.x=1.234;
+  pclGrid.fetch(1,2,3).push_back(pcl);
+  pcl.x=2.345;
+  pclGrid.fetch(1,2,3).push_back(pcl);
+  PclList_AoS<SpeciesPcl>& pclList = pclGrid.fetch(1,2,3);
+  //SpeciesPcl& pcl0 = pclList[0];
+  printf(
+    "pclGrid[%d][%d][%d][%d].x = %g\n",
+    1,2,3,0,pclList[0]);
+  printf(
+    "pclGrid[%d][%d][%d][%d].x = %g\n",
+    1,2,3,1,pclList[1]);
+}
+
+int main()
+{
+  printf("hello world\n");
+  test_PclGrid();
+}
