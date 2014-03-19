0bc452dd30ab2d9cdff787e315c3d76523cf6610
Sebastian Rinke <s.rinke@grs-sim.de>
2014-03-19 16:26:21 +0100

    Code + instructions for split execution on Xeon and Xeon Phi.

--- build.xeonphi/run_fields.sh
+++ build.xeonphi/run_fields.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/apps/intel/13/composer_xe_2013_sp1.1.106/compiler/lib/intel64
+
+# Set number of OpenMP threads per spawned MPI proc 
 export OMP_NUM_THREADS=4
+
 ../build.xeon/exec/iPic3D_fields $1
