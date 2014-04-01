2ac6082091b48e923794527adbd06477f8d92afb
eajohnson <e.alec.johnson@gmail.com>
2014-04-01 22:17:02 +0200

    added more tests for mic_particles.h

--- tests/test_mic_particles.cpp
+++ tests/test_mic_particles.cpp
@@ -15,6 +15,7 @@
 #include "../utility/errors.cpp"
 
 using namespace iPic3D;
+using namespace std;
 
 const int nxc = 10;
 const int nyc = 12;
@@ -71,10 +72,59 @@ void test_get_field_components_for_cell()
   }
 }
 
+// import numpy;
+// X0 = numpy.array([.1, .2, .5, 0, .5, .5, .5, 0]);
+// #X0 = X0r[::-1];
+// X1 = 1 - X0;
+// weights0 = range(8);
+// weights0[0] = X0[0]*X0[1]*X0[2]; # weight000
+// weights0[1] = X0[0]*X0[1]*X1[2]; # weight001
+// weights0[2] = X0[0]*X1[1]*X0[2]; # weight010
+// weights0[3] = X0[0]*X1[1]*X1[2]; # weight011
+// weights0[4] = X1[0]*X0[1]*X0[2]; # weight100
+// weights0[5] = X1[0]*X0[1]*X1[2]; # weight101
+// weights0[6] = X1[0]*X1[1]*X0[2]; # weight110
+// weights0[7] = X1[0]*X1[1]*X1[2]; # weight111   
+// 
+void test_construct_weights_for_2pcls()
+{
+  F64vec8 X0(0,.1,.2,.5,0,.5,.2,.1);
+  F64vec8 weights[2];
+  construct_weights_for_2pcls(weights, X0);
+  for(int i=0;i<8;i++)
+  {
+    std::cout << "weights0[" << i << "]=" << weights[0][i] << endl;
+  }
+  for(int i=0;i<8;i++)
+  {
+    std::cout << "weights1[" << i << "]=" << weights[1][i] << endl;
+  }
+}
+
+void test_maximum()
+{
+  F64vec8 u(1,-2,4,5,-6,7,-8,-9);
+  F64vec8 v(1,2,-4,5,-6,7,8,-9);
+  F64vec8 mx = maximum(u,v);
+  F64vec8 mn = minimum(u,v);
+  cout << "u=" << u << endl;
+  cout << "v=" << v << endl;
+  cout << "mx=" << mx << endl;
+  cout << "mn=" << mn << endl;
+}
+
 int main()
 {
   printf("#\n");
   printf("# calling test_get_field_components_for_cell:\n");
   printf("#\n");
   test_get_field_components_for_cell();
+  printf("#\n");
+  printf("# calling test_construct_weights_for_2pcls:\n");
+  printf("#\n");
+  test_construct_weights_for_2pcls();
+  printf("#\n");
+  printf("# calling test_maximum:\n");
+  printf("#\n");
+  test_maximum();
 }
